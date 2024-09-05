e2ca886b6e0f10209f0907f1231f33381a836fea
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro-2.local>
2024-09-06 03:32:02 +0900

    refacter

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import { useTranslation } from "../i18n";
 import Marquee from "react-fast-marquee";
 import { Button } from "@/components/ui/button";
-import ResponsiveImage from "../../components/common/ResponsiveImage";
+import ResponsiveImage from "@/components/common/ResponsiveImage";
+import RowSection from "@/components/common/RowSection";
 
 export default async function Home({
   params: { lang },
@@ -58,6 +59,26 @@ export default async function Home({
           </div>
         </div>
       </div>
+      {/* Service Menu */}
+      <div className="relative min-h-[150px] md:min-h-[250px] overflow-hidden my-12 mx-5 rounded-md shadow-md">
+        <div className="absolute top-0 left-0 w-full ">
+          <Image
+            src={"/assets/images/whats-96fune.jpg"}
+            className="object-cover w-full h-auto blur-[1px] "
+            width={1920}
+            height={1080}
+            alt=""
+          />
+          <div className="absolute inset-0 bg-black opacity-30"></div>
+        </div>
+        <div className="absolute top-0 bottom-0 left-0 w-full h-full z-10 ">
+          <div className="flex justify-center items-center h-full w-full">
+            <p className="text-white tracking-widest font-bold text-lg">
+              なぜKUROFUNEが良いのか？
+            </p>
+          </div>
+        </div>
+      </div>
       {/* Marquee */}
       <div className="overflow-hidden whitespace-nowrap my-3">
         <Marquee speed={30}>
@@ -110,29 +131,197 @@ export default async function Home({
             width={120}
             height={40}
           />
+          <Image
+            className="py-1 px-2"
+            src="https://via.placeholder.com/320x120"
+            alt="Brand"
+            width={120}
+            height={40}
+          />
+          <Image
+            className="py-1 px-2"
+            src="https://via.placeholder.com/320x120"
+            alt="Brand"
+            width={120}
+            height={40}
+          />
+          <Image
+            className="py-1 px-2"
+            src="https://via.placeholder.com/320x120"
+            alt="Brand"
+            width={120}
+            height={40}
+          />
+          <Image
+            className="py-1 px-2"
+            src="https://via.placeholder.com/320x120"
+            alt="Brand"
+            width={120}
+            height={40}
+          />
+          <Image
+            className="py-1 px-2"
+            src="https://via.placeholder.com/320x120"
+            alt="Brand"
+            width={120}
+            height={40}
+          />
+          <Image
+            className="py-1 px-2"
+            src="https://via.placeholder.com/320x120"
+            alt="Brand"
+            width={120}
+            height={40}
+          />
         </Marquee>
       </div>
-      {/* Service Menu */}
-      <div className="relative h-[150px] overflow-hidden m-4 rounded-md shadow-md">
-        <div className="absolute top-0 left-0 w-full ">
-          <Image
-            src={"/assets/images/whats-96fune.jpg"}
-            className="object-cover w-full h-auto blur-[1px] "
-            width={1920}
-            height={1080}
-            alt=""
-          />
-          <div className="absolute inset-0 bg-black opacity-30"></div>
-        </div>
-        <div className="absolute top-0 bottom-0 left-0 w-full h-full z-10 ">
-          <div className="flex justify-center items-center h-full w-full">
-            <p className="text-white tracking-widest font-bold text-lg">
-              なぜKUROFUNEが良いのか？
+      {/* Service Content - 1 */}
+      <div className="bg-gray-100">
+        <div className="relative w-full py-8  max-w-[1200px] m-auto md:flex md:justify-center md:items-center md:gap-5 md:p-5">
+          <div className="md:w-[50%]">
+            <Image
+              src={"https://via.placeholder.com/1920x1080"}
+              width={1920}
+              height={1080}
+              alt="test image"
+            />
+          </div>
+          <div className="p-3 my-5 tracking-wider md:w-[50%]">
+            <h4 className="font-bold text-3xl">
+              400万人の優秀層が
+              <br />
+              ｢想い｣で通じあえる
+              <br />
+              会社を待っています。
+            </h4>
+            <p className="mt-5  text-sm">
+              Wantedlyには価値観を軸にシゴトを探すユーザーが幅広い職種から集まっています。仕事のやりがいや、事業の社会的意義の発信を通じて、自社に本当にマッチする人材を採用しましょう。
             </p>
           </div>
         </div>
       </div>
-      <div className="relative"></div>
+
+      {/* Service Content - 2 */}
+      <div className="relative pt-6">
+        <div className="text-center mb-10">
+          <h4 className="text-3xl font-extrabold">
+            会社の「想い」が、
+            <br />
+            見える、伝わる。
+          </h4>
+          <p>
+            あなたの会社だけの魅力を伝える
+            <br />
+            豊富な手段が揃っています。
+          </p>
+        </div>
+        <RowSection
+          heading="掲載し放題の募集で"
+          desc="中途からインターンまで、公開本数の限りなくあらゆる職種で応募を集めることができます。誰でも簡単に会社の想いを伝えることができるフォーマットを提供しています。"
+          bgText="Hello World World"
+          btnLabel="募集を開始"
+          imgSrc="https://via.placeholder.com/1920x1080"
+          subText="新卒・インターン / 中途・副業採用 / 全職種対応"
+          flexReverse={true}
+        />
+        <RowSection
+          heading="掲載し放題の募集で"
+          desc="中途からインターンまで、公開本数の限りなくあらゆる職種で応募を集めることができます。誰でも簡単に会社の想いを伝えることができるフォーマットを提供しています。"
+          bgText="Hello World World"
+          btnLabel="募集を開始"
+          imgSrc="https://via.placeholder.com/1920x1080"
+          subText="新卒・インターン / 中途・副業採用 / 全職種対応"
+          flexReverse={false}
+        />
+        <RowSection
+          heading="掲載し放題の募集で"
+          desc="中途からインターンまで、公開本数の限りなくあらゆる職種で応募を集めることができます。誰でも簡単に会社の想いを伝えることができるフォーマットを提供しています。"
+          bgText="Hello World"
+          btnLabel="募集を開始"
+          imgSrc="https://via.placeholder.com/1920x1080"
+          subText="新卒・インターン / 中途・副業採用 / 全職種対応"
+          flexReverse={true}
+        />
+      </div>
+      {/* Service Card */}
+      <div className="relative pt-6 pb-14 px-5 w-full max-w-[1200px] m-auto">
+        <div className="w-full bg-blue-400 py-6  px-3 rounded-xl shadow-md text-white md:flex md:justify-around md:items-center">
+          <div className="md:w-[60%]">
+            <h3 className="text-2xl md:text-4xl font-bold mb-5 leading-loose">
+              成果報酬なし。
+              <br /> 採用コストを抑えられます。
+            </h3>
+            <p className="my-6 font-light leading-loose">
+              Wantedlyは、運用の工夫で採用単価が抑えられ、使えば使うほどお客様に採用効果を感じていただけるよう、成果報酬のかからない毎月定額の料金モデルを採用しています。
+            </p>
+          </div>
+          <div className="md:w-[30%]">
+            <Button
+              variant={"outline"}
+              className="bg-transparent w-full rounded-full md:bg-white md:text-gray-500 font-bold tracking-wider"
+            >
+              料金表を見る
+            </Button>
+          </div>
+        </div>
+      </div>
+      {/* Service Menu */}
+      <div className="relative -z-20 bg-gray-50 py-12">
+        <div className="w-full max-w-pagewidth m-auto p-5">
+          <div className="relative w-full h-auto">
+            <div className="absolute top-12 md:top-3 left-0 right-0 -z-10">
+              <p className="text-center text-7xl md:text-9xl md:scale-150 font-bold text-blue-100">
+                OPTIONS
+              </p>
+            </div>
+            <h4 className="text-center font-bold text-3xl md:text-5xl">
+              採用ニーズに合わせて カスタマイズ。
+            </h4>
+          </div>
+          <div className="grid grid-cols-3 gap-3 mt-14 md:mt-40">
+            <div className="flex flex-col justify-center items-center">
+              <div className="bg-gray-100 w-[100px] md:w-[180px] h-[100px] md:h-[180px] flex"></div>
+              <span className="text-xs md:text-base text-gray-500 mt-2 tracking-wide text-center px-2">
+                hello world
+              </span>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <div className="bg-gray-100 w-[100px] md:w-[180px] h-[100px] md:h-[180px] flex"></div>
+              <span className="text-xs md:text-base text-gray-500 mt-2 tracking-wide text-center px-2">
+                hello world
+              </span>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <div className="bg-gray-100 w-[100px] md:w-[180px] h-[100px] md:h-[180px] flex"></div>
+              <span className="text-xs md:text-base text-gray-500 mt-2 tracking-wide text-center px-2">
+                hello world
+              </span>
+            </div>
+            <div className="w-full flex justify-evenly items-center  col-span-3">
+              <div className="flex flex-col justify-center items-center">
+                <div className="bg-gray-100 w-[100px] md:w-[180px] h-[100px] md:h-[180px] flex "></div>
+                <span className="text-xs md:text-base text-gray-500 mt-2 tracking-wide text-center px-2">
+                  hello world
+                </span>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <div className="bg-gray-100 w-[100px] md:w-[180px] h-[100px] md:h-[180px] flex "></div>
+                <span className="text-xs md:text-base text-gray-500 mt-2 tracking-wide text-center px-2">
+                  hello world
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex justify-center items-center pt-3 my-8">
+            <Button
+              variant={"outline"}
+              className="font-bold text-gray-500 bg-transparent rounded-full py-2 px-5 md:px-12"
+            >
+              オプションについて詳しく
+            </Button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ components/common/RowSection.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+interface RowSectionProps {
+  heading: string;
+  desc: string;
+  bgText: string;
+  btnLabel: string;
+  imgSrc: string;
+  subText?: string;
+  alt?: string;
+  flexReverse?: boolean;
+}
+
+const RowSection = ({
+  heading,
+  desc,
+  bgText,
+  btnLabel,
+  imgSrc,
+  subText,
+  alt,
+  flexReverse,
+}: RowSectionProps) => {
+  return (
+    <div
+      className={`w-full max-w-pagewidth m-auto overflow-hidden px-5 py-14  md:flex md:justify-center md:items-center md:gap-8 ${
+        flexReverse ? "md:flex-row-reverse" : ""
+      }`}
+    >
+      <div className="relative tracking-wider mb-10 md:mb-0">
+        <div
+          className={`absolute top-0 left-0 -z-10 rotate-90 ${
+            flexReverse ? "translate-x-[50%]" : "-translate-x-[50%]"
+          }`}
+        >
+          <div className="flex justify-center items-center w-full h-auto translate-x-[25%]">
+            <p className="text-center text-7xl font-extrabold text-blue-100">
+              {bgText}
+            </p>
+          </div>
+        </div>
+        <p className="text-3xl font-bold">{heading}</p>
+        <span className="block py-5">{desc}</span>
+        {subText && <span className="block py-5">{subText}</span>}
+        <Button className="mt-8 px-12 rounded-full">{btnLabel}</Button>
+      </div>
+      <div className="shadow-sm">
+        <Image
+          src={imgSrc}
+          width={1920}
+          height={1080}
+          alt={alt ? alt : heading}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RowSection;
